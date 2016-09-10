ecdea5583b8968088c573406418c213549adf166
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2016-09-10 19:04:59 -0400

    solve part 1

--- day3/hw2.rkt
+++ day3/hw2.rkt
@@ -1,35 +1,41 @@
 #lang racket
 
-;;; Student Name: Frankly Olin [change to your name]
+;;; Student Name: Dakota
 ;;;
 ;;; Check one:
 ;;; [ ] I completed this assignment without assistance or external resources.
-;;; [ ] I completed this assignment with assistance from ___
-;;;     and/or using these external resources: ___
+;;; [X] I completed this assignment with using these external resources: one page racket cheatsheet
 
 ;;; 1.  Create a calculator that takes one argument: a list that represents an expression.
 
 (define (calculate x)
-  your-code-here)
+  (if (eq? (first x) 'ADD)
+    (+ (first (rest x)) (first (rest (rest x))))
+    (if (eq? (first x) 'SUB)
+      (- (first (rest x)) (first (rest (rest x))))
+      (display "broken")
+    )
+  )
+)
 
 (calculate '(ADD 3 4)) ;; --> 7
 
-;;; 2. Expand the calculator's operation to allow for arguments that are themselves well-formed arithmetic expressions.
-
-(calculate '(ADD 3 (MUL 4 5))) ;; --> 23   ;; what is the equivalent construction using list?
-(calculate '(SUB (ADD 3 4) (MUL 5 6))) ;; --> -23
-
-;;; 3. Add comparators returning booleans (*e.g.*, greater than, less than, …).
-;; Note that each of these takes numeric arguments (or expressions that evaluate to produce numeric values),
-;; but returns a boolean.  We suggest operators `GT`, `LT`, `GE`, `LE`, `EQ`, `NEQ`.
-
-	(calculate '(GT (ADD 3 4) (MUL 5 6))) ;; --> #f
-	(calculate '(LE (ADD 3 (MUL 4 5)) (SUB 0 (SUB (ADD 3 4) (MUL 5 6))))) ;; --> #t
-
-;;; 4. Add boolean operations ANND, ORR, NOTT
-
-(calculate '(AND (GT (ADD 3 4) (MUL 5 6))) (LE (ADD 3 (MUL 4 5)) (SUB 0 (SUB (ADD 3 4) (MUL 5 6)))))) ;; --> #f
-
-;;; 5. Add IPH
-
-(calculate '(IPH (GT (ADD 3 4) 7) (ADD 1 2) (ADD 1 3))) ;; -> 4
+; ;;; 2. Expand the calculator's operation to allow for arguments that are themselves well-formed arithmetic expressions.
+;
+; (calculate '(ADD 3 (MUL 4 5))) ;; --> 23   ;; what is the equivalent construction using list?
+; (calculate '(SUB (ADD 3 4) (MUL 5 6))) ;; --> -23
+;
+; ;;; 3. Add comparators returning booleans (*e.g.*, greater than, less than, …).
+; ;; Note that each of these takes numeric arguments (or expressions that evaluate to produce numeric values),
+; ;; but returns a boolean.  We suggest operators `GT`, `LT`, `GE`, `LE`, `EQ`, `NEQ`.
+;
+; (calculate '(GT (ADD 3 4) (MUL 5 6))) ;; --> #f
+; (calculate '(LE (ADD 3 (MUL 4 5)) (SUB 0 (SUB (ADD 3 4) (MUL 5 6))))) ;; --> #t
+;
+; ;;; 4. Add boolean operations ANND, ORR, NOTT
+;
+; (calculate '(AND (GT (ADD 3 4) (MUL 5 6))) (LE (ADD 3 (MUL 4 5)) (SUB 0 (SUB (ADD 3 4) (MUL 5 6))))) ;; --> #f
+;
+; ;;; 5. Add IPH
+;
+; (calculate '(IPH (GT (ADD 3 4) 7) (ADD 1 2) (ADD 1 3))) ;; -> 4

3c9fb42e01f4cbd503e883422debda02e1ba6be2
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2016-09-10 20:10:07 -0400

    solve part 2

--- day3/hw2.rkt
+++ day3/hw2.rkt
@@ -9,22 +9,29 @@
 ;;; 1.  Create a calculator that takes one argument: a list that represents an expression.
 
 (define (calculate x)
-  (if (eq? (first x) 'ADD)
-    (+ (first (rest x)) (first (rest (rest x))))
-    (if (eq? (first x) 'SUB)
-      (- (first (rest x)) (first (rest (rest x))))
-      (display "broken")
+  ;(display x) (newline)
+  (if (number? x)
+    x
+    (if (eq? (first x) 'ADD)
+      (+ (calculate (first (rest x))) (calculate (first (rest (rest x)))))
+      (if (eq? (first x) 'SUB)
+        (- (calculate (first (rest x))) (calculate (first (rest (rest x)))))
+        (if (eq? (first x) 'MUL)
+          (* (calculate (first (rest x))) (calculate (first (rest (rest x)))))
+          (display "broken")
+        )
+      )
     )
   )
 )
 
 (calculate '(ADD 3 4)) ;; --> 7
 
-; ;;; 2. Expand the calculator's operation to allow for arguments that are themselves well-formed arithmetic expressions.
-;
-; (calculate '(ADD 3 (MUL 4 5))) ;; --> 23   ;; what is the equivalent construction using list?
-; (calculate '(SUB (ADD 3 4) (MUL 5 6))) ;; --> -23
-;
+;;; 2. Expand the calculator's operation to allow for arguments that are themselves well-formed arithmetic expressions.
+
+(calculate '(ADD 3 (MUL 4 5))) ;; --> 23   ;; what is the equivalent construction using list?
+(calculate '(SUB (ADD 3 4) (MUL 5 6))) ;; --> -23
+
 ; ;;; 3. Add comparators returning booleans (*e.g.*, greater than, less than, …).
 ; ;; Note that each of these takes numeric arguments (or expressions that evaluate to produce numeric values),
 ; ;; but returns a boolean.  We suggest operators `GT`, `LT`, `GE`, `LE`, `EQ`, `NEQ`.
